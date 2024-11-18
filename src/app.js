f0c3deba5fc3c0cc322e42654c3887ec0775cb6a
Deepak <aryanrajj1234@gmail.com>
2024-11-18 17:38:48 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -3,12 +3,35 @@ const express = require("express");
 // we create instance of express.js application
 const app = express();
 
-// this will only handle GET call to /user
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log("params:", req.params)
-  res.send({ firstName: "Deepak", lastName: "Kumar" });
-});
+app.use(
+  "/user",[
+  (req, res, next) => {
+    // route handler
+    
+    console.log("route handler user 1");
+    next();
+    // res.send("route handler 1");
+  },
+  (req, res,next) => {
+    // route handler
+    // res.send("route handler 2");
+    console.log("route handler user 2");
+    next();
+  },],
+  (req, res,next) => {
+    // route handler
+    // res.send("route handler 3");
+    console.log("route handler user 3");
+    next();
+  },
+  (req, res,next) => {
+    // route handler
+    // res.send("route handler 4");
+    console.log("route handler user 4");
+    next();
+  },
+);
 
-app.listen(3000, () => {
+app.listen(8888, () => {
   console.log("server is successfully listening on port 3000...");
 });
