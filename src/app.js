eb4d84d39cb997426f74770e8809a3ee08d85ff7
Deepak <aryanrajj1234@gmail.com>
2024-11-20 16:13:07 +0530

    api get , post

--- src/app.js
+++ src/app.js
@@ -5,17 +5,71 @@ const User = require("./models/user");
 const app = express();
 
 app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  // we creating a new instance of the user model
+  // Creating a new instance of the User model
   const user = new User(req.body);
+
   try {
     await user.save();
-    res.send("User created successfully");
+    res.send({ message: "User created successfully", user });
   } catch (err) {
-    res.status(500).send("error creating user", err.message);
+    // console.error("Error creating user:", err.message);
+    res
+      .status(500)
+      .send({ message: "Error creating user", error: err.message });
   }
+});
 
-  console.log(req.body);
+// one user find from the database
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  console.log("User email:", userEmail);
+
+  try {
+    const user = await User.find({
+      emailId: userEmail,
+    });
+
+    if (user.length > 0) {
+      res.status(200).send({ message: "User successfully found", user });
+    } else {
+      res.status(404).send({ message: "User not found" });
+    }
+  } catch (err) {
+    console.error("Error:", err.message);
+    res.status(500).send({ message: "An error occurred", error: err.message });
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const userName = req.body.firstName;
+
+  console.log("User Name:", userName);
+
+  try {
+    const user = await User.findOne({
+      firstName: userName,
+    });
+
+    res.status(200).send({ message: "User successfully found", user });
+  } catch (err) {
+    console.error("Error:", err.message);
+    res.status(500).send({ message: "An error occurred", error: err.message });
+  }
+});
+
+// fead api - get all the user from the database
+app.get("/feed", async (req, res) => {
+  try {
+    // it will get all data
+    const users = await User.find({});
+    res.json(users);
+  } catch (err) {
+    res.status(500).send("error getting find all users", err.message);
+  }
 });
 
 // Connect to the database and run the server
--- src/models/user.js
+++ src/models/user.js
@@ -1,32 +1,23 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName:{
-        type: String,
-        required: true
-    },
-    lastName:{
-        type: String,
-        required: true
-    },
-    emailId:{
-        type: String,
-        required: true, 
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true,
-        minlength: 8
-    },
-    age:{
-        type: Number,
-        // required: true,
-    },
-    gender:{
-        type: String,
-        // required: true
-    }
-})
-module.exports = mongoose.model('User',userSchema);
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
+});
+module.exports = mongoose.model("User", userSchema);
