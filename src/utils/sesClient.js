8d1a581da8f1e446feda28e20264a78735007434
Deepak <aryanrajj1234@gmail.com>
2025-01-11 16:55:16 +0530

    update backend code

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -2,6 +2,9 @@ const { SESClient} = require("@aws-sdk/client-ses");
 require('dotenv').config();
 
 
+console.log(process.env.AWS_SES_REGION)
+
+
 const sesClient = new SESClient({
   region: process.env.AWS_SES_REGION,
   credentials: {
